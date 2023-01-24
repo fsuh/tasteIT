5f24ccb0201864ffd19fb501b54dc66b563cc995
Fuh Suh <tofuhsuh@gmail.com>
2023-01-24 23:59:43 +0200

    update console.log statements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import { Link } from 'react-router-dom';
-import styles from '../styles/Card.module.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styles from "../styles/Card.module.css";
 
-const Card = ({id, image, name, country}) => {
+const Card = ({ id, image, name, country }) => {
+  const [flagArr, setFlagArr] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [flagArr, setFlagArr] = useState('');
-  const [isLoading, setIsLoading] =useState(false)
-  
+  useEffect(() => {
+    setIsLoading(false);
+    axios
+      .get("https://restcountries.com/v3.1/name/" + country)
+      .then((res) => setFlagArr(res.data[0].flags.svg))
+      .catch((err) => alert("Loading of list of Countries Was not successful"));
 
-  useEffect(()=>{
-    setIsLoading(false)
-    axios.get('https://restcountries.com/v3.1/name/' + country)
-    .then((res) => setFlagArr(res.data[0].flags.svg))
-    .catch((err) => console.log(err.message))
+    setIsLoading(false);
+  }, [country]);
 
-    setIsLoading(false)
-  },[country])
-
-  if(isLoading){
-    return <p>Loading...</p>
+  if (isLoading) {
+    return <p>Loading...</p>;
   }
   return (
     <div className={styles.card}>
-        <div className='recipe'>
-            <img src={image} alt={name} />
-        </div>
-        <div className={styles.flag}>
-            <img src={flagArr} alt={country} />
-        </div>
-        
-        <h3>{name}</h3>
-        <Link to={`${id}`}>see more...</Link>
+      <div className="recipe">
+        <img src={image} alt={name} />
+      </div>
+      <div className={styles.flag}>
+        <img src={flagArr} alt={country} />
+      </div>
 
+      <h3>{name}</h3>
+      <Link to={`${id}`}>see more...</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/Addnew.jsx
+++ src/pages/Addnew.jsx
@@ -1,182 +1,169 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import styles from '../styles/Addnew.module.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "../styles/Addnew.module.css";
 
 const Addnew = () => {
   const [recipe, setRecipe] = useState({
-    name:'',
-    author:'',
-    origin:'',
-    flag:'',
-    description:'',
-    image:'',
-    ingredients:[
+    name: "",
+    author: "",
+    origin: "",
+    flag: "",
+    description: "",
+    image: "",
+    ingredients: [
       {
-        item:"",
-        quantity:""
-      }
+        item: "",
+        quantity: "",
+      },
     ],
-    instructions:''
+    instructions: "",
+  });
 
-  })
+  const [ingredientFields, setIngredientFields] = useState([
+    {
+      item: "",
+      quantity: "",
+    },
+  ]);
 
-  const[ingredientFields, setIngredientFields]=useState([{
-    item:'', quantity:''
-}])
+  const [countryList, setCountryList] = useState([{ name: "" }]);
+  const [isLoading, setIsLoading] = useState(false);
 
-const [countryList, setCountryList] =useState([{name:''}])
-const [isLoading, setIsLoading] = useState(false)
-
-useEffect(()=>{
-  setIsLoading(true)
-  axios.get('https://restcountries.com/v3.1/all')
-  .then((response) =>{
-      let countries = [];
-      response.data.map((item) => {
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        let countries = [];
+        response.data.map((item) => {
           const name = item.name.common;
-          let temp ={name};
-         countries.push(temp)
-         countries.sort((a,b) => a.name > b.name? 1: -1)
-         return(countries)
+          let temp = { name };
+          countries.push(temp);
+          countries.sort((a, b) => (a.name > b.name ? 1 : -1));
+          return countries;
+        });
+        setCountryList(countries);
+        setIsLoading(false);
       })
-      setCountryList(countries)
-      setIsLoading(false)
-  })
-  .catch((err)=> console.log(err.message))
-  
-},[])
+      .catch((err) => alert("Loading of list of countries was not successful"));
+  }, []);
 
-if(isLoading){
-  return <p>Loading...</p>
-}
-
-const addFields =(e) =>{
-  e.preventDefault();
-  let object = {
-      item:'',
-      quantity:''
+  if (isLoading) {
+    return <p>Loading...</p>;
   }
-  setIngredientFields([...ingredientFields, object])
-}
 
+  const addFields = (e) => {
+    e.preventDefault();
+    let object = {
+      item: "",
+      quantity: "",
+    };
+    setIngredientFields([...ingredientFields, object]);
+  };
 
   const addIngredients = (e, index) => {
     let ingredientValues = [...ingredientFields];
-    ingredientValues[index][e.target.name]=e.target.value;
+    ingredientValues[index][e.target.name] = e.target.value;
     setIngredientFields(ingredientValues);
-    setRecipe({...recipe, ingredients:ingredientFields})
-  }
+    setRecipe({ ...recipe, ingredients: ingredientFields });
+  };
 
-  const changeHandle = e => {
-    setRecipe({...recipe, [e.target.name]:e.target.value})
-  }
+  const changeHandle = (e) => {
+    setRecipe({ ...recipe, [e.target.name]: e.target.value });
+  };
 
-  const submitHandler = e => {
-    e.preventDefault()
-    postHandler()
-    window.location.reload()
+  const submitHandler = (e) => {
+    e.preventDefault();
+    postHandler();
+    window.location.reload();
+  };
 
-  }
-
-  const postHandler =async () => {
+  const postHandler = async () => {
     try {
-      
-     await axios.post('http://localhost:3001/recipe', recipe);
-    } catch (error) {
-      
-    }
-    
-  }
+      await axios.post("http://localhost:3001/recipe", recipe);
+    } catch (error) {}
+  };
 
-  
   return (
     <div className={styles.addnew}>
-      <form className={styles.form} onSubmit={submitHandler} >
-    <h1>Add new recipe</h1>
-        <label 
-        htmlFor='name'>Name:
-        <input 
-        type='text' 
-        name='name'
-        onChange={changeHandle}
-        required
-        /></label>
-        <label 
-        htmlFor='author'>Author:
-        <input 
-        type='text'  
-        name='author'
-        autoComplete='off'
-        onChange={changeHandle}
-        /></label>
-        <label 
-        htmlFor='origin'>Recipe is from:
+      <form className={styles.form} onSubmit={submitHandler}>
+        <h1>Add new recipe</h1>
+        <label htmlFor="name">
+          Name:
+          <input type="text" name="name" onChange={changeHandle} required />
         </label>
-        <select name='origin' defaultValue={"Select"} onChange={changeHandle}>
-        <option value="Select" disabled={true}>Select a country</option>
-        {countryList && countryList
-        .map(item =>{
-            return (<option key={item.name} value={item.name}>{item.name}</option>)
-        })}
+        <label htmlFor="author">
+          Author:
+          <input
+            type="text"
+            name="author"
+            autoComplete="off"
+            onChange={changeHandle}
+          />
+        </label>
+        <label htmlFor="origin">Recipe is from:</label>
+        <select name="origin" defaultValue={"Select"} onChange={changeHandle}>
+          <option value="Select" disabled={true}>
+            Select a country
+          </option>
+          {countryList &&
+            countryList.map((item) => {
+              return (
+                <option key={item.name} value={item.name}>
+                  {item.name}
+                </option>
+              );
+            })}
         </select>
-        <label 
-        htmlFor='description'>Description:
+        <label htmlFor="description">Description:</label>
+        <textarea name="description" onChange={changeHandle}></textarea>
+        <label htmlFor="image">
+          image:
+          <input
+            type="text"
+            name="image"
+            autoComplete="off"
+            onChange={changeHandle}
+          />
         </label>
-        <textarea 
-        name='description'
-        onChange={changeHandle}>
-        </textarea>
-        <label 
-        htmlFor='image'>image:
-        <input 
-        type='text' 
-        name='image'
-        autoComplete='off'
-        onChange={changeHandle}
-        /></label>
-        <label 
-        htmlFor='ingredients'>Ingredients:
-        </label>
+        <label htmlFor="ingredients">Ingredients:</label>
         <div className={styles.form2}>
-        {ingredientFields.map((ingredients, index)=>{
-        return(
-            <div key={index}>
-            <label 
-            htmlFor='item'>Item:
-            <input 
-            type='text'  
-            name='item'
-            value={ingredients.item}
-            onChange={(e) => addIngredients(e,index)}
-            /></label>
-            <label 
-            htmlFor='quantity'>Quantity:
-            <input 
-            type='text' 
-            name='quantity'
-            value={ingredients.quantity}
-            onChange={(e) => addIngredients(e,index)}
-            /></label>
-            </div>
-        )
-        })}
-        <button 
-        onClick={addFields}>add more</button>
+          {ingredientFields.map((ingredients, index) => {
+            return (
+              <div key={index}>
+                <label htmlFor="item">
+                  Item:
+                  <input
+                    type="text"
+                    name="item"
+                    value={ingredients.item}
+                    onChange={(e) => addIngredients(e, index)}
+                  />
+                </label>
+                <label htmlFor="quantity">
+                  Quantity:
+                  <input
+                    type="text"
+                    name="quantity"
+                    value={ingredients.quantity}
+                    onChange={(e) => addIngredients(e, index)}
+                  />
+                </label>
+              </div>
+            );
+          })}
+          <button onClick={addFields}>add more</button>
         </div>
-        <label 
-        htmlFor='instructions'>Instructions:
-        </label>
-        <textarea name='instructions'
-        onChange={changeHandle}>
-        </textarea>
-        <input 
-        type='submit' 
-        value='Post recipe'
-        // onClick={submitHandler}
+        <label htmlFor="instructions">Instructions:</label>
+        <textarea name="instructions" onChange={changeHandle}></textarea>
+        <input
+          type="submit"
+          value="Post recipe"
+          // onClick={submitHandler}
         />
-    </form>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Addnew
+export default Addnew;
