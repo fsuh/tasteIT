b9e42c2aa05ed99a9cbf44227d4b1debb3bb2fe4
Fuh Suh <tofuhsuh@gmail.com>
2023-01-25 00:02:28 +0200

    delete console.log statements

--- src/pages/Recipies.jsx
+++ src/pages/Recipies.jsx
@@ -1,76 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import Card from '../components/Card';
-import SearchBar from '../components/SearchBar';
-import styles from '../styles/Recipies.module.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "../components/Card";
+import SearchBar from "../components/SearchBar";
+import styles from "../styles/Recipies.module.css";
 
 const Recipies = () => {
-
-  const [data, setData] =useState([])
-  const [searchInput, setSearchInput] = useState('');
-  const [isLoading, setIsLoading]=useState(false);
+  const [data, setData] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   // const [flagArr, setFlagArr] = useState()
 
+  useEffect(() => {
+    setIsLoading(true);
 
-  useEffect(()=>{
-    setIsLoading(true)
+    axios
+      .get("http://localhost:3001/recipe")
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) =>
+        alert("access to recipe server wasn't successful, retry!!")
+      );
+  }, []);
 
-    axios.get('http://localhost:3001/recipe')
-    .then((res)=>{
-      setData(res.data);
-      setIsLoading(false)
-    })
-    .catch((err)=>console.log(err.message));
-
-    
-  },[])
-
-  if(isLoading){
-    return <p>Loading...</p>
+  if (isLoading) {
+    return <p>Loading...</p>;
   }
 
-  const handleChange = e =>{
-    setSearchInput(e.target.value)
-    
-  }
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submit')
-  }
+  };
 
-  const search = (data) =>{
+  const search = (data) => {
     return data.filter(
       (item) =>
-      item.name.toLowerCase().includes(searchInput.toLowerCase())||
-      item.origin.toLowerCase().includes(searchInput.toLowerCase()))
-  }
+        item.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        item.origin.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  };
 
   return (
     <div className={styles.recipes}>
-      <SearchBar 
+      <SearchBar
         submit={handleSubmit}
         onChange={handleChange}
         value={searchInput}
       />
 
-    <div className={styles.results}>
-
-    {search(data).map((item) =>(
-      <Card key={item.id}
-        image={item.image}
-        name={item.name}
-        country={item.origin}
-        id ={item.id}
-
-      />
-
-
-    ))}
-
+      <div className={styles.results}>
+        {search(data).map((item) => (
+          <Card
+            key={item.id}
+            image={item.image}
+            name={item.name}
+            country={item.origin}
+            id={item.id}
+          />
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Recipies
+export default Recipies;
